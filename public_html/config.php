609aeeefc330f8a1ea9888643c2ded7f73c0d140
Jarir Ahmed Deedat <jarircse16@gmail.com>
2023-08-26 13:32:15 +0600

    Fixed SQL injection Bug

--- public_html/config.php
+++ public_html/config.php
@@ -1,4 +1,6 @@
 <?php
+require_once "jarir_ddos_waf.php";
+require_once "jarir_sqli_ssi_rfi_lfi_xss_waf.php";
 $hostname = "https://news478.000webhostapp.com/";
 
 $conn = mysqli_connect("localhost","id21083165_root","AbCd1234!","id21083165_news_site") or die("Connection failed : " . mysqli_connect_error());
